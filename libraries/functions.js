d1e89815e79ab1f1e7c8f49e7034c00d4ad09c8c
Dumps Like A Truck <corbin@dumpslikeatruckwaste.com>
2025-01-09 11:19:40 -0800

    Update functions.js

--- libraries/functions.js
+++ libraries/functions.js
@@ -31,7 +31,6 @@ const now = new Date();
 //Convert Unix UTC Milliseconds to MM-DD-YYYY
 function convertUnixToDate(unixMilliseconds) {
     const date = new Date(unixMilliseconds); // Convert milliseconds to Date object
-
     const day = String(date.getUTCDate()).padStart(2, '0'); // Get day and pad with leading zero
     const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Get month (0-based) and pad with leading zero
     const year = date.getUTCFullYear(); // Get year
@@ -45,7 +44,6 @@ function calculateDaysBetweenDates(date1,date2) {
   if (date1 && date2) {
     const unixTime1 = new Date(date1).getTime();
     const unixTime2 = new Date(date2).getTime();
-
     const millisecondsPerDay = 24 * 60 * 60 * 1000;
     const differenceInMilliseconds = Math.abs(unixTime1 - unixTime2);
 
@@ -67,7 +65,6 @@ function convertDateFormat(dateString) {
 // Convert Phone Number
 function formatPhoneNumber(phoneNumber) {
     const cleaned = phoneNumber.replace(/\D/g, "");
-
     const areaCode = cleaned.slice(0, 3);
     const firstPart = cleaned.slice(3, 6);
     const secondPart = cleaned.slice(6, 10);
@@ -90,7 +87,6 @@ function stringToInteger(str) {
 function sortEntriesDescending() {
     const list = document.getElementById("entryList");
     const entries = Array.from(list.querySelectorAll("[data-date]"));
-
     entries.sort((a, b) => {
         const dateA = parseInt(a.getAttribute("data-date"), 10);
         const dateB = parseInt(b.getAttribute("data-date"), 10);
@@ -120,14 +116,13 @@ function fade() {
 }
 
 function hideWithDelay(element, duration = 250) {
-  const now = Date.now();
-
-  if (!firstDisplayedTimestamp) {
-    firstDisplayedTimestamp = now;
-  }
-  const elapsedTime = now - firstDisplayedTimestamp;
-  const delay = Math.max(0, 1100 - elapsedTime);
-  setTimeout(() => fadeOut(element, duration), delay);
+    const now = Date.now();
+    if (!firstDisplayedTimestamp) {
+        firstDisplayedTimestamp = now;
+    }
+    const elapsedTime = now - firstDisplayedTimestamp;
+    const delay = Math.max(0, 1100 - elapsedTime);
+    setTimeout(() => fadeOut(element, duration), delay);
 }
 
 //
