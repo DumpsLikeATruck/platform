3a9a48d010d286d2be269fd0aa8f584361e8461d
Dumps Like A Truck <corbin@dumpslikeatruckwaste.com>
2025-01-09 14:54:42 -0800

    Update loader.js

--- libraries/loader.js
+++ libraries/loader.js
@@ -1,1 +1,25 @@
+function fadeOut(element, duration = 250) {
+  let opacity = 1; 
+  const interval = 10; 
+  const gap = interval / duration; 
 
+function fade() {
+    opacity -= gap;
+    element.style.opacity = opacity;
+    if (opacity <= 0) {
+      element.style.display = 'none'; 
+      clearInterval(fading);
+    }
+  }
+  const fading = setInterval(fade, interval);
+}
+
+function hideWithDelay(element, duration = 250) {
+    const now = Date.now();
+    if (!firstDisplayedTimestamp) {
+        firstDisplayedTimestamp = now;
+    }
+    const elapsedTime = now - firstDisplayedTimestamp;
+    const delay = Math.max(0, 1100 - elapsedTime);
+    setTimeout(() => fadeOut(element, duration), delay);
+}
